df40fa4d46bf149da2bec81bf1aca2f6d90f6c18
Jesse Roe <jessedroe@gmail.com>
2015-10-23 18:44:02 -0400

    Initial commit

---
+++ BinarySearchTree.hpp
@@ -0,0 +1,246 @@
+
+/**
+ * Binary Search Tree (expanded definition)
+ */
+
+#ifndef BINARY_SEARCH_TREE_H
+#define BINARY_SEARCH_TREE_H
+
+#include <iostream>
+#include <set>
+#include <vector>
+using namespace std;
+
+template <typename T>
+
+class BinarySearchTree {
+  private:                     /* private data */
+    struct gv_node;            /* forward declaration */
+    gv_node *root;
+
+  public:
+    BinarySearchTree () {
+        root = nullptr;
+    }
+
+    ~BinarySearchTree () {
+        removeAll (root);       /* start emptying the tree from the root */
+    }
+
+    //--- accessor functions ---//
+    bool isEmpty () const {
+        return root == nullptr;
+    }
+
+    bool contains (const T & x) const {
+        return contains (x, root);
+    }
+    
+    const T & findMin () const {
+        return findMin (root)->data;
+    }
+    
+    const T & findMax () const {
+        return findMax (root)->data;
+    }
+    
+    void print (std::ostream & destination = std::cout) const {
+        print (destination, root);
+    }
+
+    //--- mutator functions ---//
+    void insert (const T & x) {
+        _insert (x, root);
+    }
+
+    void remove (const T & x) {
+        remove (x, root);
+    }
+
+    int number_of_nodes() const {
+        return _number_of_nodes(root);
+    }
+
+    int number_of_leaves() const {
+        return _number_of_leaves(root);
+    }
+
+    int number_of_full_nodes() const {
+        return _number_of_full_nodes(root);
+    }
+
+    /* Remove all the leaf nodes in the tree */
+    std::set<T> remove_leaves() {
+        std::set<T> col;
+        auto point = &col;
+        _remove_leaves(root, point);
+        return col;
+    }
+
+    vector<T> get_range (const T& k1, const T& k2) const {
+        vector<T> result;
+//        _get_range(root, result, k1, k2);
+        return result;
+    }
+
+  private:
+    /*!!! Notice how _insert and remove are const member functions !!! */
+
+    void _insert (const T & x, gv_node * &t) const {
+        if (t == nullptr) {
+            t = new gv_node;
+            t->data = x;
+            t->left = t->right = nullptr;
+        }
+        else if (x < t->data)
+            _insert (x, t->left);
+        else if (x > t->data)
+            _insert (x, t->right);
+        else
+            /* attempted to insert a duplicate item ... */
+            ;
+    }
+
+    void remove (const T & x, gv_node * &t) const {
+        if (t == nullptr)
+            return;
+
+        if (x < t->data)
+            remove (x, t->left);
+        else if (x > t->data)
+            remove (x, t->right);
+        else {
+            /* we found the node to delete */
+            if (t->left != NULL && t->right != NULL) // TWO children
+            {
+                /* find the smallest value in the right subtree,
+                 * and replace the current node with that value */
+                t->data = findMin (t->right)->data;
+                remove (t->data, t->right);
+            }
+            else {              // one child or none
+                gv_node *me = t;
+                /* promote the child node */
+                t = (t->left != NULL) ? t->left : t->right;
+                delete me;
+            }
+        }
+    }
+
+    bool contains (const T & x, gv_node * t) const {
+        if (t == nullptr)
+            return false;
+
+        if (x < t->data)
+            return contains (x, t->left);
+        if (x > t->data)
+            return contains (x, t->right);
+        return true;
+    }
+    
+    void removeAll (gv_node * &t) {
+        if (t == nullptr)
+            return;
+        removeAll (t->left);    /* empty the left sub-tree */
+        removeAll (t->right);   /* empty the right subtree */
+        delete t;               /* delete self */
+        t = nullptr;
+    }
+
+    void print (std::ostream & dest, gv_node * t, int depth = 0) const {
+        if (t == nullptr)
+            return;
+        for (int k = 0; k < depth - 1; k++) // for indentation
+            dest << "|   ";
+        if (depth > 0)
+            dest << "+---";
+        dest << t->data << std::endl;
+        print (dest, t->left, depth + 1);
+        print (dest, t->right, depth + 1);
+    }
+
+    gv_node *findMin (gv_node * t) const {
+        if (t == nullptr)
+            return nullptr;
+
+        /* locate the leftmost node with no left child */
+        while (t->left != nullptr)
+            t = t->left;
+        return t;
+    }
+    
+    gv_node *findMax (gv_node * t) const {
+        if (t == nullptr)
+            return nullptr;
+
+        /* locate the rightmost leaf node with no right child */
+        while (t->right != nullptr)
+            t = t->right;
+        return t;
+    }
+
+    int _number_of_nodes (gv_node * t) const {
+        if (t == nullptr)
+            return 0;
+        else {
+            return (1 + _number_of_nodes(t->left) + _number_of_nodes(t->right));
+        }
+    }
+
+    int _number_of_leaves (gv_node * t) const {
+        if(t == nullptr)
+            return 0;
+        if(t->right == nullptr && t->left == nullptr)
+            return 1;
+        else {
+            return (_number_of_leaves(t->right) + _number_of_leaves(t->left));
+        }
+    }
+
+    int _number_of_full_nodes(gv_node * t) const {
+        if(t == nullptr)
+            return 0;
+        if(t->right != nullptr && t->left != nullptr)
+            return (1 + _number_of_full_nodes(t->right) + _number_of_full_nodes(t->left));
+        else {
+            return (_number_of_full_nodes(t->right) + _number_of_full_nodes(t->left));
+        }
+    }
+
+    void _remove_leaves(gv_node * &node, set<T> * &s) {
+        if (node == nullptr) {
+            return;
+        }
+        if (node->right == nullptr && node->left == nullptr){
+            s->insert(node->data);
+            remove(node->data, node);
+            return;
+        }
+        _remove_leaves(node->right, s);
+        _remove_leaves(node->left, s);
+    }
+
+     void _get_range(vector<T> &result , gv_node * curr, const int k1, const int k2) const{
+
+            if(curr == nullptr){
+                return;
+            }
+
+            if(curr->data >= k1 && curr->data <= k2){
+                result.push_back(curr->data);
+            }
+            _get_range(result, curr->left, k1,k2);
+            _get_range(result, curr->right,k1,k2);
+        }
+
+};
+
+/*---------------------------------------------------*
+ *   The gv_node structure (Page 126, lines 22-30)   *
+ *---------------------------------------------------*/
+template < typename T > struct BinarySearchTree <T >::gv_node {
+    T data;
+    gv_node *left;
+    gv_node *right;
+};
+#endif
---
+++ hw04_main.cpp
@@ -0,0 +1,64 @@
+#include "BinarySearchTree.hpp"
+#include "gvsu_cis.h"
+
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+TEST_CASE("HW04 of nodes")
+{
+    vector<int> data {50, 25, 78, 15, 66, 92, 9, 20, 63, 76, 95, 68};
+    BinarySearchTree<int> t1;
+    for (auto x : data)
+        t1.insert (x);
+
+    SECTION ("Node counting") {
+        cout << data.size() << endl;
+        cout << t1.number_of_nodes() << endl;
+        REQUIRE (t1.number_of_nodes() == data.size());
+        cout << 5 << endl;
+        cout << t1.number_of_leaves() << endl;
+        REQUIRE (t1.number_of_leaves() == 5);
+        cout << 4 << endl;
+        cout << t1.number_of_full_nodes() << endl;
+        REQUIRE (t1.number_of_full_nodes() == 4);
+    }
+
+    SECTION ("Range") {
+        auto r1 = t1.get_range(51, 60);
+        REQUIRE (r1.size() == 0);
+        auto r2 = t1.get_range (100, 200);
+        REQUIRE (r2.size() == 0);
+        auto r3 = t1.get_range (-10, -7);
+        REQUIRE (r3.size() == 0);
+
+        auto r4 = t1.get_range(1, 30);
+        REQUIRE (r4.size() == 4);
+        REQUIRE (r4[0] == 9);
+        REQUIRE (r4[3] == 25);
+
+        auto r5 = t1.get_range (17, 80);
+        REQUIRE (r5.size() == 8);
+        REQUIRE (r5[0] == 20);
+        REQUIRE (r5[2] == 50);
+        REQUIRE (r5[7] == 78);
+    }
+
+    SECTION ("Remove Leaves") {
+        int before, after;
+        int leaves;
+        vector<int> testvals {20, 15, 66, 78, 50};
+        int k = 0;
+        while (!t1.isEmpty()) {
+            before = t1.number_of_nodes();
+            leaves = t1.number_of_leaves();
+            auto r = t1.remove_leaves();
+            after = t1.number_of_nodes();
+            REQUIRE (after < before);
+            REQUIRE (r.size() == leaves);
+            auto iter = find (r.begin(), r.end(), testvals[k]);
+            REQUIRE (iter != r.end());
+            k++;
+        }
+    }
+}
